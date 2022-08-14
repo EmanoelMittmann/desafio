a839f9bffdbd98ce1d25cbe4104e59ed91edc3ef
unknown <emanoelmittmann@hotmail.com>
2022-08-14 11:37:25 -0300

    context_api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import './App.css';
+import {Link} from 'react-router-dom'
 
 function App() {
+  const [users, setUsers] = useState('')
+  const [response, setResponse] = useState('')
+
+  const style = {
+    boxShadow:'0 0 5px blue',
+    width: '500px',
+    margin: 'auto',
+    borderRadius:'10px',
+    textAlign: 'center',
+    cursor:'pointer'
+  }
+
+  
+  const HandleSubmit = (e) => {
+    e.preventDefault()
+    axios
+    .get(`https://api.github.com/users/${users}`)
+    .then(res => setResponse(res.data))
+    .catch(err => console.log(err))
+  }
+  
+  const handleClick = (e) => {
+    e.preventDefault()
+    window.location.replace(`https://github.com/${users}`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={HandleSubmit}>
+        <input 
+          placeholder='Digite seu usuario' 
+          value={users} 
+          onChange={(e) => setUsers(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+      </form>
+      
+    {response ? 
+      <div style={style} onClick={handleClick}>
+          <h2>Name: {response.name}</h2>
+          <p>Numero de repositorios: {response.public_repos}</p>
+          <i>{response.bio}</i>
+      </div>
+      :null    
+    }
+
     </div>
   );
 }
